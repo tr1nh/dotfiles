ca9baf8b161e81125c2696d335a37373c0a8a7c8
Khánh Trình <trandiepkhanhtrinh@gmail.com>
2024-05-21 12:45:46 +0700

    Updated i3 and compton config

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -3,10 +3,20 @@
 # wallpaper:
 ~/.fehbg &
 
+# compositor
+# compton&
+
 # mute volume:
 pactl set-sink-volume 0 0% &
 
 # enable tapping on synaptics touchpad:
-xinput set-prop $(xinput list | grep Synaptics | awk '{print $5}' | cut -d '=' -f 2) 'libinput Tapping Enabled' 1 &
+xinput set-prop $(xinput list | grep Touchpad | awk '{print $6}' | cut -d '=' -f 2) 'libinput Tapping Enabled' 1 &
+
+# notification daemon
 dunst &
 
+# resolution
+xrandr --output eDP-1 --mode 1368x768
+
+# brightness
+brightness 600
